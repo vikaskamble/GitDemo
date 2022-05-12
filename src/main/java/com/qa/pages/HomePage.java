07a44eb36cb6bc7dd9512533cf8b0a9081b14041
vikas <vikas.kamble@afourtech.com>
2022-05-12 06:15:38 -0700

    test home page

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -5,4 +5,8 @@ public class HomePage{
     public void homePage(){
         System.out.println( "This is home Page." );
     }
+
+    public void testHomePage(){
+        System.out.println( "Test home Page." );
+    }
 }
